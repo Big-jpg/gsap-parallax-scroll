127aa3c2cc49b21d01953d1945684350d29834c6
Big-jpg <56429528+Big-jpg@users.noreply.github.com>
2025-10-21 14:15:24 -0400

    Fix ScrollTrigger refresh and animation sync issues

--- components/ParallaxScroll.tsx
+++ components/ParallaxScroll.tsx
@@ -4,247 +4,254 @@ import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
+}
 
 export default function ParallaxScroll() {
+  const containerRef = useRef<HTMLDivElement>(null);
   const svgRef = useRef<SVGSVGElement>(null);
-  const scrollElementRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (!svgRef.current || !scrollElementRef.current) return;
+    if (!containerRef.current || !svgRef.current) return;
 
-    const speed = 100;
-    const height = svgRef.current.getBBox().height;
+    const ctx = gsap.context(() => {
+      const speed = 100;
+      const height = 500;
 
-    // Initial setup
-    gsap.set("#h2-1", { opacity: 0 });
-    gsap.set("#bg_grad", { attr: { cy: "-50" } });
-    gsap.set("#scene3", { y: height - 40, visibility: "visible" });
+      // Initial setup
+      gsap.set("#h2-1", { opacity: 0 });
+      gsap.set("#bg_grad", { attr: { cy: "-50" } });
+      gsap.set("#scene3", { y: height - 40, visibility: "visible" });
 
-    const mm = gsap.matchMedia();
-    mm.add("(max-width: 1922px)", () => {
-      gsap.set(["#cloudStart-L", "#cloudStart-R"], { x: 10, opacity: 1 });
-    });
+      const mm = gsap.matchMedia();
+      mm.add("(max-width: 1922px)", () => {
+        gsap.set(["#cloudStart-L", "#cloudStart-R"], { x: 10, opacity: 1 });
+      });
 
-    /* SCENE 1 */
-    const scene1 = gsap.timeline();
-    ScrollTrigger.create({
-      animation: scene1,
-      trigger: ".scrollElement",
-      start: "top top",
-      end: "45% 100%",
-      scrub: 3,
-    });
+      /* SCENE 1 */
+      const scene1 = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".scrollElement",
+          start: "top top",
+          end: "45% 100%",
+          scrub: 3,
+          invalidateOnRefresh: true,
+        },
+      });
 
-    // Hills animation
-    scene1.to("#h1-1", { y: 3 * speed, x: 1 * speed, scale: 0.9, ease: "power1.in" }, 0);
-    scene1.to("#h1-2", { y: 2.6 * speed, x: -0.6 * speed, ease: "power1.in" }, 0);
-    scene1.to("#h1-3", { y: 1.7 * speed, x: 1.2 * speed }, 0.03);
-    scene1.to("#h1-4", { y: 3 * speed, x: 1 * speed }, 0.03);
-    scene1.to("#h1-5", { y: 2 * speed, x: 1 * speed }, 0.03);
-    scene1.to("#h1-6", { y: 2.3 * speed, x: -2.5 * speed }, 0);
-    scene1.to("#h1-7", { y: 5 * speed, x: 1.6 * speed }, 0);
-    scene1.to("#h1-8", { y: 3.5 * speed, x: 0.2 * speed }, 0);
-    scene1.to("#h1-9", { y: 3.5 * speed, x: -0.2 * speed }, 0);
-    scene1.to("#cloudsBig-L", { y: 4.5 * speed, x: -0.2 * speed }, 0);
-    scene1.to("#cloudsBig-R", { y: 4.5 * speed, x: -0.2 * speed }, 0);
-    scene1.to("#cloudStart-L", { x: -300 }, 0);
-    scene1.to("#cloudStart-R", { x: 300 }, 0);
-    scene1.to("#info", { y: 8 * speed }, 0);
+      // Hills animation
+      scene1.to("#h1-1", { y: 3 * speed, x: 1 * speed, scale: 0.9, ease: "power1.in" }, 0);
+      scene1.to("#h1-2", { y: 2.6 * speed, x: -0.6 * speed, ease: "power1.in" }, 0);
+      scene1.to("#h1-3", { y: 1.7 * speed, x: 1.2 * speed }, 0.03);
+      scene1.to("#h1-4", { y: 3 * speed, x: 1 * speed }, 0.03);
+      scene1.to("#h1-5", { y: 2 * speed, x: 1 * speed }, 0.03);
+      scene1.to("#h1-6", { y: 2.3 * speed, x: -2.5 * speed }, 0);
+      scene1.to("#h1-7", { y: 5 * speed, x: 1.6 * speed }, 0);
+      scene1.to("#h1-8", { y: 3.5 * speed, x: 0.2 * speed }, 0);
+      scene1.to("#h1-9", { y: 3.5 * speed, x: -0.2 * speed }, 0);
+      scene1.to("#cloudsBig-L", { y: 4.5 * speed, x: -0.2 * speed }, 0);
+      scene1.to("#cloudsBig-R", { y: 4.5 * speed, x: -0.2 * speed }, 0);
+      scene1.to("#cloudStart-L", { x: -300 }, 0);
+      scene1.to("#cloudStart-R", { x: 300 }, 0);
+      scene1.to("#info", { y: 8 * speed }, 0);
 
-    /* Bird */
-    gsap.fromTo(
-      "#bird",
-      { opacity: 1 },
-      {
-        y: -250,
-        x: 800,
-        ease: "power2.out",
+      /* Bird */
+      gsap.fromTo(
+        "#bird",
+        { opacity: 1 },
+        {
+          y: -250,
+          x: 800,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: ".scrollElement",
+            start: "15% top",
+            end: "60% 100%",
+            scrub: 4,
+            invalidateOnRefresh: true,
+            onEnter: () => gsap.to("#bird", { scaleX: 1, rotation: 0 }),
+            onLeave: () => gsap.to("#bird", { scaleX: -1, rotation: -15 }),
+          },
+        }
+      );
+
+      /* Clouds */
+      const clouds = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".scrollElement",
+          start: "top top",
+          end: "70% 100%",
+          scrub: 1,
+          invalidateOnRefresh: true,
+        },
+      });
+
+      clouds.to("#cloud1", { x: 500 }, 0);
+      clouds.to("#cloud2", { x: 1000 }, 0);
+      clouds.to("#cloud3", { x: -1000 }, 0);
+      clouds.to("#cloud4", { x: -700, y: 25 }, 0);
+
+      /* Sun motion Animation */
+      const sun = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".scrollElement",
+          start: "1% top",
+          end: "2150 100%",
+          scrub: 2,
+          invalidateOnRefresh: true,
+        },
+      });
+
+      sun.fromTo("#bg_grad", { attr: { cy: "-50" } }, { attr: { cy: "330" } }, 0);
+      sun.to("#bg_grad stop:nth-child(2)", { attr: { offset: "0.15" } }, 0);
+      sun.to("#bg_grad stop:nth-child(3)", { attr: { offset: "0.18" } }, 0);
+      sun.to("#bg_grad stop:nth-child(4)", { attr: { offset: "0.25" } }, 0);
+      sun.to("#bg_grad stop:nth-child(5)", { attr: { offset: "0.46" } }, 0);
+      sun.to("#bg_grad stop:nth-child(6)", { attr: { "stop-color": "#FF9171" } }, 0);
+
+      /* SCENE 2 */
+      const scene2 = gsap.timeline({
         scrollTrigger: {
           trigger: ".scrollElement",
           start: "15% top",
-          end: "60% 100%",
-          scrub: 4,
-          onEnter: function () {
-            gsap.to("#bird", { scaleX: 1, rotation: 0 });
-          },
-          onLeave: function () {
-            gsap.to("#bird", { scaleX: -1, rotation: -15 });
-          },
+          end: "40% 100%",
+          scrub: 3,
+          invalidateOnRefresh: true,
         },
-      }
-    );
+      });
 
-    /* Clouds */
-    const clouds = gsap.timeline();
-    ScrollTrigger.create({
-      animation: clouds,
-      trigger: ".scrollElement",
-      start: "top top",
-      end: "70% 100%",
-      scrub: 1,
-    });
+      scene2.fromTo("#h2-1", { y: 500, opacity: 0 }, { y: 0, opacity: 1 }, 0);
+      scene2.fromTo("#h2-2", { y: 500 }, { y: 0 }, 0.1);
+      scene2.fromTo("#h2-3", { y: 700 }, { y: 0 }, 0.1);
+      scene2.fromTo("#h2-4", { y: 700 }, { y: 0 }, 0.2);
+      scene2.fromTo("#h2-5", { y: 800 }, { y: 0 }, 0.3);
+      scene2.fromTo("#h2-6", { y: 900 }, { y: 0 }, 0.3);
 
-    clouds.to("#cloud1", { x: 500 }, 0);
-    clouds.to("#cloud2", { x: 1000 }, 0);
-    clouds.to("#cloud3", { x: -1000 }, 0);
-    clouds.to("#cloud4", { x: -700, y: 25 }, 0);
+      /* Bats */
+      gsap.set("#bats", { transformOrigin: "50% 50%" });
+      gsap.fromTo(
+        "#bats",
+        { opacity: 1, y: 400, scale: 0 },
+        {
+          y: 20,
+          scale: 0.8,
+          ease: "power3.out",
+          scrollTrigger: {
+            trigger: ".scrollElement",
+            start: "40% top",
+            end: "70% 100%",
+            scrub: 3,
+            invalidateOnRefresh: true,
+            onEnter: () => {
+              gsap.utils.toArray("#bats path").forEach((item, i) => {
+                gsap.to(item as gsap.TweenTarget, {
+                  scaleX: 0.5,
+                  yoyo: true,
+                  repeat: 9,
+                  transformOrigin: "50% 50%",
+                  duration: 0.15,
+                  delay: 0.7 + i / 10,
+                });
+              });
+              gsap.set("#bats", { opacity: 1 });
+            },
+          },
+        }
+      );
 
-    /* Sun motion Animation */
-    const sun = gsap.timeline();
-    ScrollTrigger.create({
-      animation: sun,
-      trigger: ".scrollElement",
-      start: "1% top",
-      end: "2150 100%",
-      scrub: 2,
-    });
-
-    sun.fromTo("#bg_grad", { attr: { cy: "-50" } }, { attr: { cy: "330" } }, 0);
-    sun.to("#bg_grad stop:nth-child(2)", { attr: { offset: "0.15" } }, 0);
-    sun.to("#bg_grad stop:nth-child(3)", { attr: { offset: "0.18" } }, 0);
-    sun.to("#bg_grad stop:nth-child(4)", { attr: { offset: "0.25" } }, 0);
-    sun.to("#bg_grad stop:nth-child(5)", { attr: { offset: "0.46" } }, 0);
-    sun.to("#bg_grad stop:nth-child(6)", { attr: { "stop-color": "#FF9171" } }, 0);
-
-    /* SCENE 2 */
-    const scene2 = gsap.timeline();
-    ScrollTrigger.create({
-      animation: scene2,
-      trigger: ".scrollElement",
-      start: "15% top",
-      end: "40% 100%",
-      scrub: 3,
-    });
-
-    scene2.fromTo("#h2-1", { y: 500, opacity: 0 }, { y: 0, opacity: 1 }, 0);
-    scene2.fromTo("#h2-2", { y: 500 }, { y: 0 }, 0.1);
-    scene2.fromTo("#h2-3", { y: 700 }, { y: 0 }, 0.1);
-    scene2.fromTo("#h2-4", { y: 700 }, { y: 0 }, 0.2);
-    scene2.fromTo("#h2-5", { y: 800 }, { y: 0 }, 0.3);
-    scene2.fromTo("#h2-6", { y: 900 }, { y: 0 }, 0.3);
-
-    /* Bats */
-    gsap.set("#bats", { transformOrigin: "50% 50%" });
-    gsap.fromTo(
-      "#bats",
-      { opacity: 1, y: 400, scale: 0 },
-      {
-        y: 20,
-        scale: 0.8,
-        ease: "power3.out",
+      /* Sun increase */
+      const sun2 = gsap.timeline({
         scrollTrigger: {
           trigger: ".scrollElement",
-          start: "40% top",
-          end: "70% 100%",
-          scrub: 3,
-          onEnter: function () {
-            gsap.utils.toArray("#bats path").forEach((item, i) => {
-              gsap.to(item as gsap.TweenTarget, {
-                scaleX: 0.5,
-                yoyo: true,
-                repeat: 9,
-                transformOrigin: "50% 50%",
-                duration: 0.15,
-                delay: 0.7 + i / 10,
-              });
-            });
-            gsap.set("#bats", { opacity: 1 });
-          },
+          start: "2000 top",
+          end: "5000 100%",
+          scrub: 2,
+          invalidateOnRefresh: true,
         },
-      }
-    );
+      });
 
-    /* Sun increase */
-    const sun2 = gsap.timeline();
-    ScrollTrigger.create({
-      animation: sun2,
-      trigger: ".scrollElement",
-      start: "2000 top",
-      end: "5000 100%",
-      scrub: 2,
-    });
+      sun2.to("#sun", { attr: { offset: "1.4" } }, 0);
+      sun2.to("#bg_grad stop:nth-child(2)", { attr: { offset: "0.7" } }, 0);
+      sun2.to("#sun", { attr: { "stop-color": "#ffff00" } }, 0);
+      sun2.to("#lg4 stop:nth-child(1)", { attr: { "stop-color": "#623951" } }, 0);
+      sun2.to("#lg4 stop:nth-child(2)", { attr: { "stop-color": "#261F36" } }, 0);
+      sun2.to("#bg_grad stop:nth-child(6)", { attr: { "stop-color": "#45224A" } }, 0);
 
-    sun2.to("#sun", { attr: { offset: "1.4" } }, 0);
-    sun2.to("#bg_grad stop:nth-child(2)", { attr: { offset: "0.7" } }, 0);
-    sun2.to("#sun", { attr: { "stop-color": "#ffff00" } }, 0);
-    sun2.to("#lg4 stop:nth-child(1)", { attr: { "stop-color": "#623951" } }, 0);
-    sun2.to("#lg4 stop:nth-child(2)", { attr: { "stop-color": "#261F36" } }, 0);
-    sun2.to("#bg_grad stop:nth-child(6)", { attr: { "stop-color": "#45224A" } }, 0);
+      /* Transition (from Scene2 to Scene3) */
+      const sceneTransition = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".scrollElement",
+          start: "60% top",
+          end: "bottom 100%",
+          scrub: 3,
+          invalidateOnRefresh: true,
+        },
+      });
 
-    /* Transition (from Scene2 to Scene3) */
-    const sceneTransition = gsap.timeline();
-    ScrollTrigger.create({
-      animation: sceneTransition,
-      trigger: ".scrollElement",
-      start: "60% top",
-      end: "bottom 100%",
-      scrub: 3,
-    });
+      sceneTransition.to("#h2-1", { y: -height - 100, scale: 1.5, transformOrigin: "50% 50%" }, 0);
+      sceneTransition.to("#bg_grad", { attr: { cy: "-80" } }, 0.0);
+      sceneTransition.to("#bg2", { y: 0 }, 0);
 
-    sceneTransition.to("#h2-1", { y: -height - 100, scale: 1.5, transformOrigin: "50% 50%" }, 0);
-    sceneTransition.to("#bg_grad", { attr: { cy: "-80" } }, 0.0);
-    sceneTransition.to("#bg2", { y: 0 }, 0);
+      /* Scene 3 */
+      const scene3 = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".scrollElement",
+          start: "70% 50%",
+          end: "bottom 100%",
+          scrub: 3,
+          invalidateOnRefresh: true,
+        },
+      });
 
-    /* Scene 3 */
-    const scene3 = gsap.timeline();
-    ScrollTrigger.create({
-      animation: scene3,
-      trigger: ".scrollElement",
-      start: "70% 50%",
-      end: "bottom 100%",
-      scrub: 3,
-    });
+      scene3.fromTo("#h3-1", { y: 300 }, { y: -550 }, 0);
+      scene3.fromTo("#h3-2", { y: 800 }, { y: -550 }, 0.03);
+      scene3.fromTo("#h3-3", { y: 600 }, { y: -550 }, 0.06);
+      scene3.fromTo("#h3-4", { y: 800 }, { y: -550 }, 0.09);
+      scene3.fromTo("#h3-5", { y: 1000 }, { y: -550 }, 0.12);
+      scene3.fromTo("#stars", { opacity: 0 }, { opacity: 0.5, y: -500 }, 0);
+      scene3.fromTo("#arrow2", { opacity: 0 }, { opacity: 0.7, y: -710 }, 0.25);
+      scene3.fromTo("#text2", { opacity: 0 }, { opacity: 0.7, y: -710 }, 0.3);
+      scene3.to("#bg2-grad", { attr: { cy: 600 } }, 0);
+      scene3.to("#bg2-grad", { attr: { r: 500 } }, 0);
 
-    scene3.fromTo("#h3-1", { y: 300 }, { y: -550 }, 0);
-    scene3.fromTo("#h3-2", { y: 800 }, { y: -550 }, 0.03);
-    scene3.fromTo("#h3-3", { y: 600 }, { y: -550 }, 0.06);
-    scene3.fromTo("#h3-4", { y: 800 }, { y: -550 }, 0.09);
-    scene3.fromTo("#h3-5", { y: 1000 }, { y: -550 }, 0.12);
-    scene3.fromTo("#stars", { opacity: 0 }, { opacity: 0.5, y: -500 }, 0);
-    scene3.fromTo("#arrow2", { opacity: 0 }, { opacity: 0.7, y: -710 }, 0.25);
-    scene3.fromTo("#text2", { opacity: 0 }, { opacity: 0.7, y: -710 }, 0.3);
-    scene3.to("#bg2-grad", { attr: { cy: 600 } }, 0);
-    scene3.to("#bg2-grad", { attr: { r: 500 } }, 0);
+      /* Falling star */
+      gsap.set("#fstar", { y: -400 });
+      const fstarTL = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".scrollElement",
+          start: "4200 top",
+          end: "6000 bottom",
+          scrub: 2,
+          invalidateOnRefresh: true,
+          onEnter: () => gsap.set("#fstar", { opacity: 1 }),
+          onLeave: () => gsap.set("#fstar", { opacity: 0 }),
+        },
+      });
+      fstarTL.to("#fstar", { x: -700, y: -250, ease: "power2.out" }, 0);
 
-    /* Falling star */
-    gsap.set("#fstar", { y: -400 });
-    const fstarTL = gsap.timeline();
-    ScrollTrigger.create({
-      animation: fstarTL,
-      trigger: ".scrollElement",
-      start: "4200 top",
-      end: "6000 bottom",
-      scrub: 2,
-      onEnter: function () {
-        gsap.set("#fstar", { opacity: 1 });
-      },
-      onLeave: function () {
-        gsap.set("#fstar", { opacity: 0 });
-      },
-    });
-    fstarTL.to("#fstar", { x: -700, y: -250, ease: "power2.out" }, 0);
+      // Twinkling stars
+      const starPaths = [1, 3, 5, 8, 11, 15, 17, 18, 25, 28, 30, 35, 40, 45, 48];
+      const delays = [0.8, 1.8, 1, 1.2, 0.5, 2, 1.1, 1.4, 1.1, 0.9, 1.3, 2, 0.8, 1.8, 1];
+      starPaths.forEach((index, i) => {
+        gsap.fromTo(
+          `#stars path:nth-of-type(${index})`,
+          { opacity: 0.3 },
+          { opacity: 1, duration: 0.3, repeat: -1, repeatDelay: delays[i] }
+        );
+      });
 
-    // Twinkling stars
-    const starPaths = [1, 3, 5, 8, 11, 15, 17, 18, 25, 28, 30, 35, 40, 45, 48];
-    const delays = [0.8, 1.8, 1, 1.2, 0.5, 2, 1.1, 1.4, 1.1, 0.9, 1.3, 2, 0.8, 1.8, 1];
-    starPaths.forEach((index, i) => {
-      gsap.fromTo(
-        `#stars path:nth-of-type(${index})`,
-        { opacity: 0.3 },
-        { opacity: 1, duration: 0.3, repeat: -1, repeatDelay: delays[i] }
-      );
-    });
+      // Force refresh after all animations are set up
+      ScrollTrigger.refresh();
+    }, containerRef);
 
-    // Cleanup
     return () => {
-      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
-      mm.revert();
+      ctx.revert();
     };
   }, []);
 
   return (
-    <div className="wrapper relative">
+    <div ref={containerRef} className="wrapper relative">
       <svg
         ref={svgRef}
         className="parallax fixed top-0 left-0 w-full h-screen z-[3] block"
@@ -546,7 +553,7 @@ export default function ParallaxScroll() {
         </g>
       </svg>
 
-      <div ref={scrollElementRef} className="scrollElement absolute top-0 w-full h-[6000px] z-[4]" />
+      <div className="scrollElement absolute top-0 w-full h-[6000px] z-[4]" />
     </div>
   );
 }
